babb45baf3b843eeff6b0f2fd9252fb9bc36f4e0
Nima Miri <1381.n.m@gmail.com>
2023-06-05 12:45:05 +0330

    added DH_params service and refactor

--- auth-server/server/server.go
+++ auth-server/server/server.go
@@ -12,18 +12,35 @@ import (
 )
 
 type reqPqServer struct {
-	pb.UnimplementedReqPqServer
+	pb.UnimplementedReq_PQServer
 }
 
-func (c *reqPqServer) RequestPQ(ctx context.Context, in *pb.Request) (*pb.Response, error) {
+func (c *reqPqServer) RequestPQ(ctx context.Context, in *pb.PQRequest) (*pb.PQResponse, error) {
 	fmt.Println("Got the request")
-	return &pb.Response{Nonce: in.GetNonce(), ServerNonce: "server_nonce", MessageId: in.GetMessageId() + 1, P: 23, G: 5}, nil
+	return &pb.PQResponse{Nonce: in.GetNonce(), ServerNonce: "server_nonce", MessageId: in.GetMessageId() + 1, P: 23, G: 5}, nil
 }
 
-func newServer() *reqPqServer {
+func newPQServer() *reqPqServer {
 	s := &reqPqServer{}
 	return s
 }
+
+////////////////////////////////second service
+
+type  reqDHParamsServer struct {
+	pb.UnimplementedReq_DH_ParamsServer
+}
+
+func (c *reqDHParamsServer) RequestDHparams(ctx context.Context, in *pb.DHRequest) (*pb.DHResponse, error) {
+	fmt.Println("got the public key")
+	return &pb.DHResponse{Nonce: in.GetNonce() , ServerNonce: in.GetServerNonce() , MessageId: in.GetMessageId() + 1 , B: 22} , nil
+}
+
+func newDHServer() *reqDHParamsServer {
+	s := &reqDHParamsServer{}
+	return s
+}
+
 
 var (
 	port = flag.Int("port", 8080, "The server port")
@@ -40,7 +57,8 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
-	pb.RegisterReqPqServer(grpcServer, newServer())
+	pb.RegisterReq_PQServer(grpcServer, newPQServer())
+	pb.RegisterReq_DH_ParamsServer(grpcServer, newDHServer())
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
