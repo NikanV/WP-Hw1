3b4c9edf64b9bf7f0eb44ca4b68530bd7665c3e2
NikanV <nikanvsiuni@gmail.com>
2023-06-05 12:53:38 +0330

    added the basics for redis

--- auth-server/server/server.go
+++ auth-server/server/server.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net"
 
+	"github.com/redis/go-redis/v9"
 	"google.golang.org/grpc"
 )
 
@@ -25,12 +26,28 @@ func newServer() *reqPqServer {
 	return s
 }
 
+func initRedisClient() {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	if err := redisClient.Set(context.Background(), "key", "value", 0).Err(); err != nil {
+		log.Fatalf("failed to set key: %v", err)
+	} else {
+		fmt.Println("successfully set key on redis")
+	}
+}
+
 var (
 	port = flag.Int("port", 8080, "The server port")
 )
 
 func main() {
 	flag.Parse()
+
+	initRedisClient()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
 	if err != nil {
