fd3d0d2892682a4535beb76ccacbe8e59c92675b
NikanV <nikanvsiuni@gmail.com>
2023-06-18 21:44:37 +0330

    refactored error handlings and directories

--- auth-server/server.go
+++ auth-server/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -24,29 +23,16 @@ type authServer struct {
 
 func (c *authServer) AuthCheck(ctx context.Context, in *pb.ACRequest) (*pb.ACResponse, error) {
 	message_id := in.GetMessageId()
-	nonce := in.GetNonce()
-	server_nonce := in.GetServerNonce()
 	auth_key := in.GetAuthKey()
-	if len(nonce) != 20 || len(server_nonce) != 20 {
-		return nil, errors.New("ACRequest : wrong nonce or server_nonce format")
-	} else if message_id%2 != 0 || message_id <= 0 {
-		return nil, errors.New("ACRequest : wrong message_id format")
-	}
+
 	client := initRedisClient()
 	defer client.Close()
-	hash := tools.Sha1_gen(nonce + server_nonce)
-	server_auth_key_str, err := client.Get(context.Background(), hash).Result()
-	if err == redis.Nil {
-		return nil, errors.New("no key found")
-	} else if err != nil {
-		return nil, errors.New("wrong key format")
-	}
-	server_auth_key, _ := strconv.ParseInt(server_auth_key_str, 10, 64)
-	fmt.Printf("the server-auth-key is : %d\n", server_auth_key)
+
+	auth_check, _ := client.Exists(context.Background(), strconv.Itoa(int(auth_key))).Result()
 
 	response := pb.ACResponse{
 		MessageId: message_id + 1,
-		AuthCheck: server_auth_key == auth_key,
+		AuthCheck: auth_check > 0,
 	}
 
 	return &response, nil
@@ -55,11 +41,7 @@ func (c *authServer) AuthCheck(ctx context.Context, in *pb.ACRequest) (*pb.ACRes
 func (c *authServer) RequestPQ(ctx context.Context, in *pb.PQRequest) (*pb.PQResponse, error) {
 	message_id := in.GetMessageId()
 	nonce := in.GetNonce()
-	if len(nonce) != 20 {
-		return nil, errors.New("PQRequest : wrong nonce format")
-	} else if message_id%2 != 0 || message_id <= 0 {
-		return nil, errors.New("PQRequest : wrong message_id format")
-	}
+
 	prime := tools.Random_Prime()
 	p, g := int64(prime), int64(tools.FindPrimitive(prime))
 	server_nonce := tools.RandomString(20)
@@ -76,9 +58,8 @@ func (c *authServer) RequestPQ(ctx context.Context, in *pb.PQRequest) (*pb.PQRes
 	if err != nil {
 		return nil, err
 	}
-	client.Expire(context.Background(), hash, 2*time.Minute)
+	client.Expire(context.Background(), hash, 20*time.Minute)
 
-	fmt.Println(in)
 	response := pb.PQResponse{
 		Nonce:       nonce,
 		ServerNonce: server_nonce,
@@ -94,30 +75,22 @@ func (c *authServer) RequestDHParams(ctx context.Context, in *pb.DHRequest) (*pb
 	nonce := in.GetNonce()
 	server_nonce := in.GetServerNonce()
 	a := in.GetA()
-	if len(nonce) != 20 || len(server_nonce) != 20 {
-		return nil, errors.New("DHRequest : wrong nonce or server_nonce format")
-	} else if message_id%2 != 0 || message_id <= 0 {
-		return nil, errors.New("DHRequest : wrong message_id format")
-	}
+
 	private_key := tools.RandomNumber(10)
-	fmt.Println(private_key)
 	hash := tools.Sha1_gen(nonce + server_nonce)
-	fmt.Println(hash)
 	client := initRedisClient()
 	defer client.Close()
 
 	p, _ := strconv.ParseInt(client.HGet(context.Background(), hash, "p").Val(), 10, 64)
 	g, _ := strconv.ParseInt(client.HGet(context.Background(), hash, "g").Val(), 10, 64)
-	fmt.Println(int64(p), math.Pow(float64(a), float64(private_key)), float64(a), float64(private_key))
 	auth_key := int64(p) % int64(math.Pow(float64(a), float64(private_key)))
+
 	client.Del(context.Background(), hash)
 	err := client.Set(context.Background(), hash, auth_key, 0).Err()
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Printf("Auth key is %d\n", auth_key)
-	fmt.Println(in)
 	response := pb.DHResponse{
 		Nonce:       nonce,
 		ServerNonce: server_nonce,
